778a2800b5d187f306be7b7d98c2822d45cc41fc
Xist <xist@xist.gg>
2023-01-12 08:57:43 -0800

    Add some functionality
    - Add -game and -editor switches
    - Add explicit read-only uproject file check with optional -force switch override
    - No longer wait for *entire -switchversion* process tree; sometimes UVS spawns a child process that hangs for a while (?)

--- UnrealVersionSelector.ps1
+++ UnrealVersionSelector.ps1
@@ -5,22 +5,28 @@
 #
 # Usage:
 #
-#   UnrealVersionSelector.ps1
-#     - Regenerate Project Files for project named
-#       the same as the current directory
+#   UnrealVersionSelector.ps1 -projectfiles
+#     - Regenerate Project Files for project in current directory
 #
-#   UnrealVersionSelector.ps1 MyGame.uproject
+#   UnrealVersionSelector.ps1 -projectfiles MyGame.uproject
 #     - Regenerate Project Files for project MyGame.uproject
 #
 
 [CmdletBinding()]
 param(
+    [switch]$Help,
+    [switch]$Editor,
+    [switch]$Game,
     [switch]$ProjectFiles,
+    [switch]$SwitchVersion,
     [switch]$SwitchVersionSilent,
     [switch]$Quiet,
+    [switch]$Force,
     [Parameter()] $UProjectFile,
     [Parameter(ValueFromRemainingArguments=$true)] $VarArgs
 )
+
+$ScriptName = $MyInvocation.MyCommand.Name
 
 
 ################################################################################
@@ -89,38 +95,127 @@ if (!$PSBoundParameters.ContainsKey('UProjectFile'))
 ###  Run Unreal Version Selector
 ################################################################################
 
-$e = 0;
+if ($SwitchVersionSilent -or $SwitchVersion)
+{
+    # When trying to switch Engine version, it requires modifying the uproject file
+    # If it's set to read-only, UVS will fail with code 1 and not give a very good error message.
+    # Check for that here and throw an explicit exception unless -force is specified,
+    # in which case forcefully remove the read only bit from the uproject file.
 
-Write-Host ""
+    if ((Get-ChildItem $UProjectFile).IsReadOnly)
+    {
+        if (!$Force)
+        {
+            throw "Your project file is read-only; check it out or make it writable, or use -force switch: $UProjectFile"
+        }
+
+        # -force flag is set; explicitly make $UProjectFile writable
+        (Get-ChildItem $UProjectFile).IsReadOnly = $false
+
+        # Print a warning message so it's obvious we did this
+        Write-Warning "-force removed read only attribute from $UProjectFile"
+    }
+}
+
+
+$process = $null
 
 if ($SwitchVersionSilent)
 {
-    Write-Host "EXEC: $UnrealVersionSelector -switchversionsilent $UProjectFile ${VarArgs[0]}"
-    $process = Start-Process -Wait -PassThru -FilePath $UnrealVersionSelector -ArgumentList "-switchversionsilent",$UProjectFile,$VarArgs[0]
-    $e = $process.ExitCode
+    # You MUST provide the Engine Dir argument for -SwitchVersionSilent
+    # It must be the path to the "Engine" folder root (its parent folder)
+    if ($VarArgs.count -eq 0)
+    {
+        Write-Error "You must specify the EngineRootDir for -SwitchVersionSilent"
+        throw "Usage: $ScriptName -switchversionsilent UProjectFile EngineRootDir"
+    }
+    $EngineDir = $VarArgs[0]
+
+    # Test $EngineDir to make sure it is a directory and it looks like
+    # it contains UE Engine/Binaries generated files
+    if (!(Test-Path -Path $EngineDir -PathType Container) -or !(Test-Path -Path "$EngineDir/Engine/Binaries" -PathType Container))
+    {
+        throw "Invalid EngineDir specified: $EngineDir"
+    }
+
+    Write-Host "EXEC: $UnrealVersionSelector -switchversionsilent $UProjectFile $EngineDir"
+    $process = Start-Process -PassThru -FilePath $UnrealVersionSelector -ArgumentList "-switchversionsilent",$UProjectFile,$EngineDir
+}
+elseif ($SwitchVersion)
+{
+    Write-Host "EXEC: $UnrealVersionSelector -switchversion $UProjectFile"
+    $process = Start-Process -PassThru -FilePath $UnrealVersionSelector -ArgumentList "-switchversion",$UProjectFile
 }
 elseif ($ProjectFiles)
 {
     Write-Host "EXEC: $UnrealVersionSelector -projectfiles $UProjectFile"
-    $process = Start-Process -Wait -PassThru -FilePath $UnrealVersionSelector -ArgumentList "-projectfiles",$UProjectFile
+    $process = Start-Process -PassThru -FilePath $UnrealVersionSelector -ArgumentList "-projectfiles",$UProjectFile
+}
+elseif ($Editor)
+{
+    Write-Host "EXEC: $UnrealVersionSelector -editor $UProjectFile"
+    $process = Start-Process -PassThru -FilePath $UnrealVersionSelector -ArgumentList "-editor",$UProjectFile -WorkingDirectory $UProjectDirectory
+}
+elseif ($Game)
+{
+    Write-Host "EXEC: $UnrealVersionSelector -game $UProjectFile"
+    $process = Start-Process -PassThru -FilePath $UnrealVersionSelector -ArgumentList "-game",$UProjectFile -WorkingDirectory $UProjectDirectory
+}
+
+
+if ($process)
+{
+    # Wait for the process we started to exit
+    # (NOTE: If it spawns child processes, we do NOT wait for those; sometimes -switchversion does funky stuff... (?))
+    Wait-Process -InputObject $process
     $e = $process.ExitCode
-}
-else
-{
-    Write-Host "Use -projectfiles to regenerate project files"
+
+    if ($e -ne 0)
+    {
+        Write-Warning "Exit Code $e"
+    }
+    else
+    {
+        Write-Host "Exit Code $e"
+    }
+
     Write-Host ""
-    exit 0
+    exit $e
 }
 
 
-if ($e -ne 0)
-{
-    Write-Warning "Exit Code $e"
-}
-else
-{
-    Write-Host "Exit Code $e"
-}
+################################################################################
+###  Help
+################################################################################
 
-Write-Host ""
-exit $e
+Write-Host @'
+
+Build Usages:
+
+    Usage: $ScriptName -projectfiles MyGame.uproject
+    Usage: $ScriptName -switchversion MyGame.uproject [-force]
+    Usage: $ScriptName -switchversionsilent MyGame.uproject /path/to/EngineRoot [-force]
+
+Play Usages:
+
+    Usage: $ScriptName -editor MyGame.uproject
+    Usage: $ScriptName -game MyGame.uproject
+
+You can sometimes omit "MyGame.uproject" from the command-line.
+It is optional if the Project Directory is named the same as the
+uproject name.  For example these project folders will work
+without having to explicitly specify "MyGame.uproject":
+
+    C:/Path/to/Game/Game.uproject
+               ^^^^ ^^^^
+
+For a project directory like the above "C:/Path/to/Game",
+the following usages will also work:
+
+    cd C:/Path/to/Game
+    $ScriptName -projectfiles
+
+    cd C:/
+    $ScriptName -projectfiles Path/to/Game
+
+'@
