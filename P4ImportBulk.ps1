a404e8af339d58d3ecfda3ececdb126638fd56ba
Xist <xist@xist.gg>
2025-01-17 07:35:01 -0800

    Improve bulk imports
    - Add auto-restart-on-crash capability (it prompts you if it seems an auto-restart is in progress)
    - Fix -ImportList on Mac/Linux not finding .p4sync.txt
    - Now confirmed working on Linux

--- P4ImportBulk.ps1
+++ P4ImportBulk.ps1
@@ -16,6 +16,24 @@
 #
 #        P4ImportBulk.ps1 -Help
 #
+#    NOTE YOU MUST SORT THE .p4sync.txt TO SUPPORT ERROR RECOVERY.
+#    If you use -CreateList to generate the .p4sync.txt it will sort for you.
+#
+#    Note that before you run -ImportList the first time, you want to
+#    MAKE SURE that the default changelist is empty.  Once this starts
+#    running, it will be using the default changelist to work.
+#
+#    Communicating with a p4 server over the public Internet is HIGHLY
+#    likely to encounter network errors while running this script, which
+#    issues MANY commands to the p4 server while processing.
+#
+#    This script includes an error recovery mode.  When you start it in
+#    -ImportList mode, if the default changelist contains files, it will
+#    ask if you want to continue with error recovery.  If you choose yes,
+#    then the contents of the default changelist are considered to be
+#    the previous work-in-progress that was aborted due to a network error,
+#    and processing will continue from that point.
+#
 
 [CmdletBinding()]
 param(
@@ -23,6 +41,7 @@ param(
     [switch]$ImportList,
     [switch]$NoParallel,
     [switch]$DryRun,
+    [switch]$DebugPrompts,
     [switch]$Help,
     [Parameter()]$BatchSize=50000,
     [Parameter()]$BucketSize=50,
@@ -63,26 +82,29 @@ Full Path = $FullPathDisplay
 
     Create $SyncFile like: "p4 add -f -n ... > $SyncFile"
 
-# THEN: if this is a brand new depot, 'p4 add .p4ignore.txt'
-# before you bulk import anything.
-
 # THEN: IMPORT THE IMPORT LIST YOU CREATED:
 & $ScriptName -ImportList
 
     Read previously-created $SyncFile and execute multiple
     "p4 submit" with reasonably-sized buckets that won't crash p4.
 
+    If (WHEN) you get network errors that cause the script to stop, just restart
+    it and it should resume where it left off after it asks you a few questions.
+
     Optional additional flags:
 
-      -BatchSize = Max paths to submit to "p4 submit" at once.
-                   50k works for me.  Too high, p4 will crash during
-                   the update.  Too low, you get tons of import CLs.
-      -BucketSize = Max number of files to include in one command line
-                    (at some point your OS complains max command line
-                    length exceeded; not much value in bumping this up)
-      -DryRun = Don't actually do anything, just a test run
-      -NoParallel = Disable parallel processing in "p4 submit" (SLOW)
-                    (Do not use this option unless your P4 server requires it)
+      -BatchSize       = Max paths to submit to "p4 submit" at once.
+                         50k works for me.  Too high, p4 will crash during
+                         the update.  Too low, you get tons of import CLs.
+      -BucketSize      = Max number of files to include in one command line
+                         (at some point your OS complains max command line
+                         length exceeded; not much value in bumping this up)
+      -DebugPrompts    = If you set this switch, you will be prompted before each
+                         `p4 add` or `p4 submit` command, which means you will be
+                         prompted A LOT. Useful for debugging.
+      -DryRun          = Don't actually do anything, just a test run
+      -NoParallel      = Disable parallel processing in "p4 submit" (SLOW)
+                         (Do not use this option unless your P4 server requires it)
 
 "@
 }
@@ -101,6 +123,8 @@ if ($CreateList)
 {
     $args = @("add", "-f", "-n", "...")
 
+    Write-Host "Generating ${SyncFile}, this might take a while..."
+
     Write-Host "EXEC: p4 $($args -join ' ') > $SyncFile"
     $process = Start-Process -NoNewWindow -PassThru -FilePath "p4" -ArgumentList $args -RedirectStandardOutput $SyncFile
 
@@ -118,6 +142,26 @@ if ($CreateList)
     {
         throw "p4 add failed"
     }
+
+    # Now, to support recovery attempts, SORT the .p4sync.txt
+    # so it's in a predictable order and we can restart reliably
+    # without losing any files due to random sort order
+
+    Write-Host "Sorting ${SyncFile} to support potential recovery efforts..."
+
+    # -Force is required for dotfiles
+    $data = Get-Content -Force -Path $SyncFile
+    # Split lines and then sort them
+    $lines = $data -split '(?:`r?`n|`r)' | Sort-Object
+    # Resave $SyncFile with sorted files
+    $lines -join [Environment]::NewLine | Set-Content -Path $SyncFile
+
+    Write-Host "Done."
+    Write-Host ""
+    Write-Host "If you are going to explicitly add your .p4ignore before you import (highly recommended)," `
+               "consider removing .p4ignore from $SyncFile for nicer looking line numbers."
+    Write-Host ""
+    Write-Host "Ready to -ImportList"
 
     $ValidUsage = $true
 }
@@ -152,6 +196,17 @@ function AddPaths()
     }
 
     Write-Host "EXEC: p4 $($args -join ' ')"
+
+    if ($DebugPrompts)
+    {
+        $userResponse = Read-Host -Prompt "Continue add? [yN] "
+        if ($userResponse -ne "y")
+        {
+            Write-Host "Cancelling prior to add based on user response."
+            exit 1
+        }
+    }
+
     $process = Start-Process -NoNewWindow -PassThru -FilePath "p4" -ArgumentList $args
 
     if (!$process)
@@ -198,6 +253,17 @@ function SubmitPaths()
     }
 
     Write-Host "EXEC: p4 $($args -join ' ')"
+
+    if ($DebugPrompts)
+    {
+        $userResponse = Read-Host -Prompt "Continue submit? [yN] "
+        if ($userResponse -ne "y")
+        {
+            Write-Host "Cancelling prior to submit based on user response."
+            exit 1
+        }
+    }
+
     $process = Start-Process -NoNewWindow -PassThru -FilePath "p4" -ArgumentList $args
 
     if (!$process)
@@ -229,6 +295,9 @@ function ImportInBatches
             $StartLine = 1
         }
 
+        $StartAfterFile = $null
+        $foundStartAfterLine = $false
+
         $LineNum = 0
         $TotalOutputLineNum = 0
 
@@ -246,14 +315,82 @@ function ImportInBatches
     {
         if (!$file)
         {
-            throw "Cannot open file: $($SyncFileItem.FullName)"
+            throw "Cannot read file: $($SyncFileItem.FullName)"
         }
+
+        ################################################################################
+        ##  Detect error recovery mode
+        ################################################################################
+
+        # Start by checking the value of the current default change, since we're using
+        # the default change for our work here.
+        $initialChange = P4_GetChange
+        if ($initialChange.Files -ne $null)
+        {
+            if (-not ($initialChange.Files -is [System.Collections.ArrayList]))
+            {
+                # Convert the string into an array
+                $initialChange.Files = @( $initialChange.Files )
+            }
+
+            # Sort the files so we can predictably recover
+            $initialChange.Files = $initialChange.Files | Sort-Object
+
+            # There are one or more files currently in the default changelist
+            $numChangeFiles = $initialChange.Files.Count
+            $lastAddedFileLine = $initialChange.Files[$numChangeFiles-1]
+
+            $lastChangeFileName = $null
+            if ($lastAddedFileLine -match '^([^#]+)#')
+            {
+                # Trim trailing spaces from the output to get the actual filename
+                $lastChangeFileName = $matches[1].TrimEnd()
+                # Note that this filename is still encoded!
+            }
+
+            if ($lastChangeFileName -eq $null)
+            {
+                Write-Error "The default changelist contains Files data that cannot be parsed"
+                throw "Programmer error - cannot parse last line: `"$lastAddedFileLine`""
+            }
+
+            Write-Host ""
+            Write-Warning "The default changelist currently contains $numChangeFiles files."
+            Write-Host ""
+            Write-Host "If you are intentionally recovering from a network error, please continue."
+            Write-Host "If you running -ImportList for the first time then STOP NOW AND CLEAN UP THE DEFAULT CHANGELIST BEFORE YOU CONTINUE."
+            Write-Host ""
+
+            $userResponse = Read-Host -Prompt "Continue with $numChangeFiles default changelist files? [yN] "
+            if ($userResponse -ne "y")
+            {
+                Write-Host "Cancelling -ImportList. The default changelist is unmodified."
+                exit 1
+            }
+
+            # By default, start after the last file that was successfully added to the default changelist
+            $StartAfterFile = $lastChangeFileName
+
+            Write-Debug "Selected StartAfterFile `"$StartAfterFile`""
+
+            # Initialize the current batch size with the existing default changelist,
+            # so we still keep the same max batch size regardless of how many we started with.
+            $CurrentBatchSize = $numChangeFiles
+        }
+
+        ################################################################################
+        ## Process lines from the file
+        ################################################################################
 
         # If we're seeking deep into the file, it might take a while,
         # which looks like the program is fucked; show that it's not.
         if ($StartLine -gt 1000)
         {
             Write-Host "Seeking to Line ${StartLine}..."
+        }
+        elseif ($StartAfterFile)
+        {
+            Write-Host "Seeking to line following `"$StartAfterFile`"..."
         }
 
         # DISABLE CTRL+C DURING `p4 submit`
@@ -276,6 +413,44 @@ function ImportInBatches
 
             $LineNum++
 
+            # Does this line represent a file we need to add?
+            $isAddLine = $line -imatch '#\d+ - opened for add$'
+            if ($isAddLine)
+            {
+                # Extract the path from this line
+                $Path = $line -ireplace '#\d+ - opened for add$',''
+
+                # `p4 add -m` writes filenames in an Encoded format, so we need to decode it
+                # see: https://www.perforce.com/manuals/cmdref/Content/CmdRef/filespecs.html
+                $DecodedPath = P4_DecodePath $Path
+            }
+
+            # To support recovery (e.g. after network errors)
+            # if $StartAfterFile is set, then don't start processing lines until we find it
+            if ($StartAfterFile -and -not $foundStartAfterLine)
+            {
+                # Compare path (encoded name) with $StartAfterFile (encoded name)
+                if ($Path -eq $StartAfterFile)
+                {
+                    Write-Host "Found StartAfterFile=`"${StartAfterFile}`" at line ${LineNum}"
+
+                    $nextLineNum = $LineNum + 1
+                    $userResponse = Read-Host -Prompt "Continue reading $($SyncFileItem.Name) on line ${nextLineNum}? [yN] "
+                    if ($userResponse -ne "y")
+                    {
+                        Write-Host "Cancelling -ImportList. The default changelist is unmodified."
+                        exit 1
+                    }
+
+                    # We've found the file we were searching for.
+                    # Start on the line AFTER this one, which is the first line not yet in the changelist.
+                    $foundStartAfterLine = $true
+                }
+
+                # Do not start yet, we need at least one more line
+                $StartLine = $LineNum + 1
+            }
+
             # Don't process this line if we're not yet supposed to start
             if ($LineNum -lt $StartLine)
             {
@@ -285,14 +460,8 @@ function ImportInBatches
             Write-Debug "${LineNum}:$line"
 
             # If the output says the file should be added, then lets add it
-            if ($line -imatch '#\d+ - opened for add$')
+            if ($isAddLine)
             {
-                $Path = $line -ireplace '#\d+ - opened for add$',''
-
-                # `p4 add -m` writes filenames in an Encoded format, so we need to decode it
-                # see: https://www.perforce.com/manuals/cmdref/Content/CmdRef/filespecs.html
-                $DecodedPath = P4_DecodePath $Path
-
                 [void] $CurrentBucket.Add($DecodedPath)
 
                 $TotalOutputLineNum++
